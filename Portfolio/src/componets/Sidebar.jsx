3362bcd7635bcf975f5d4d732bc99f3559c0b0e1
drewpayton <drewpayton18@gmail.com>
2024-07-01 10:47:21 -0500

    Adding a temporary sidebar

---
+++ Portfolio/src/componets/Sidebar.jsx
@@ -0,0 +1,16 @@
+import "../styles/Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div class="sidebar">
+        <a class="active" href="#home">
+          Home
+        </a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </>
+  );
+}
